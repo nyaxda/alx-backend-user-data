440b270d18dad1bf9c31c0abbd099327075ec0c2
dan <nyaxda@gmail.com>
2024-09-03 03:41:59 -0500

    Added auth.py file

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""auth module"""
+
+from flask import request
+from api.v1.views import app_views
+from models.user import User
+from typing import List, Type
+
+
+class Auth:
+    """Auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header"""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> Type[User]:
+        """current_user"""
+        return None
