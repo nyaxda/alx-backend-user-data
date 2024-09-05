b9bfc23246a21d31151081ddbedebc65f721b62c
dan <nyaxda@gmail.com>
2024-09-05 11:15:52 -0500

    Docstrings append

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,6 +10,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """Create a session"""
         if user_id is None or type(user_id) != str:
             return None
         session_id = str(uuid.uuid4())
@@ -17,6 +18,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Get a user ID from a session ID"""
         if session_id is None or type(session_id) != str:
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
