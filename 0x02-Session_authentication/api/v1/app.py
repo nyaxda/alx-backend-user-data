99de3514ceae1183fe0cb75bdd917869f46b648d
dan <nyaxda@gmail.com>
2024-09-05 12:14:50 -0500

    chore: Refactor session authentication logic and fix authorization issue

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,13 +37,11 @@ def before_request() -> Optional[str]:
                       '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if (auth.authorization_header(request) is None and
-        auth.session_cookie(request) is None):
-        abort(401)
     request.current_user = auth.current_user(request)
 
 
