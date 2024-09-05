1b2cb17a0f78d93a6b1601014b14c6a8c02147fe
dan <nyaxda@gmail.com>
2024-09-05 14:03:35 -0500

    chore: Add login endpoint for session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@
 from api.v1.auth.auth import Auth
 import uuid
 from models.user import User
+from flask import request, jsonify
+from api.v1.views import app_views
 
 
 class SessionAuth(Auth):
@@ -30,3 +32,23 @@ class SessionAuth(Auth):
         """
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
+
+    @app_views.route('/auth_session/login',
+                     methods=['POST'], strict_slashes=False)
+    def auth_session_login():
+        """Handles user login and session creation"""
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if not email:
+            return jsonify({"error": "email missing"}, 400)
+        if not password:
+            return jsonify({"error": "password missing"}, 400)
+        user = User.search({'email': email})
+        if not user:
+            return jsonify({"error": "no user found for this email"}, 404)
+        if not user[0].is_valid_password(password):
+            return jsonify({"error": "wrong password"}, 401)
+        session_id = SessionAuth.create_session(user[0].id)
+        response = jsonify(user[0].to_json())
+        response.set_cookie('my_session_id', session_id)
+        return response
