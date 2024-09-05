2223d04cd6565a9b0d59e2161b25d2553930a1fa
dan <nyaxda@gmail.com>
2024-09-05 14:13:57 -0500

    chore: Refactor session authentication logic and fix authorization issue

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -40,14 +40,14 @@ class SessionAuth(Auth):
         email = request.form.get('email')
         password = request.form.get('password')
         if not email:
-            return jsonify({"error": "email missing"}, 400)
+            return jsonify({"error": "email missing"}), 400
         if not password:
-            return jsonify({"error": "password missing"}, 400)
+            return jsonify({"error": "password missing"}), 400
         user = User.search({'email': email})
         if not user:
-            return jsonify({"error": "no user found for this email"}, 404)
+            return jsonify({"error": "no user found for this email"}), 404
         if not user[0].is_valid_password(password):
-            return jsonify({"error": "wrong password"}, 401)
+            return jsonify({"error": "wrong password"}), 401
         session_id = SessionAuth.create_session(user[0].id)
         response = jsonify(user[0].to_json())
         response.set_cookie('my_session_id', session_id)
