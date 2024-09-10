36da56b1663d80f25ede9f42c05b6857f144ba02
dan <nyaxda@gmail.com>
2024-09-10 12:12:27 -0500

    Refactor add_user method in db.py to accept email and hashed_password as string parameters and handle exceptions

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,10 +33,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """add a new user to the database"""
         try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
             self._session.commit()
         except Exception:
             self._session.rollback()
-            new_user = None
-        return new_user
+            user = None
+        return user
