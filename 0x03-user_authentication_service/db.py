619b4aa17a97e4f924415412f0dad4426510bf44
dan <nyaxda@gmail.com>
2024-09-10 11:57:34 -0500

    chore: Handle exception and rollback session in add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,12 @@ class DB:
 
     def add_user(self, email, hashed_password) -> User:
         """add a new user to the database"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            return user
+        except Exception:
+            self._session.rollback()
+            user = None
+            return user
