eac47aa1c147460f748ca9bd0cd98d713685955a
dan <nyaxda@gmail.com>
2024-09-10 11:15:45 -0500

    Added user.py File

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,1 @@
+# Project tasks for 0x03. User authentication service
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""user authentication module"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, VARCHAR
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session = Column(VARCHAR(250))
+    reset_token = Column(VARCHAR(250))
