034a9efcd0f7c6acf627175c520d70623c667cc8
dan <nyaxda@gmail.com>
2024-09-10 11:28:11 -0500

    chore: Update User model column types and session column name

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,10 +4,12 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+
 Base = declarative_base()
 
 
 class User(Base):
+    """ User class"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
