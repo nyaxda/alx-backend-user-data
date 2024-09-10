c295c415fef2966ab963209931ab47359659adfb
dan <nyaxda@gmail.com>
2024-09-10 11:25:04 -0500

    chore: Remove VARCHAR import and update User model column types to use String

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, VARCHAR, String
+from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
