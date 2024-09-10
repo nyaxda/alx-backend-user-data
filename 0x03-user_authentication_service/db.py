af083e279fe0692ee66298db8d23ae1c8d918c7b
dan <nyaxda@gmail.com>
2024-09-10 12:03:18 -0500

    chore: Update add_user method in db.py to accept email and hashed_password as string parameters and return User object

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """add a new user to the database"""
         try:
             user = User(email=email, hashed_password=hashed_password)
