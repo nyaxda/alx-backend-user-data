d237f178a3a04fa0d23f106c54dd83cf2f4f5523
dan <nyaxda@gmail.com>
2024-09-10 11:22:55 -0500

    chore: Update User model column types to use String instead of VARCHAR

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, VARCHAR
+from sqlalchemy import Column, Integer, VARCHAR, String
 Base = declarative_base()
 
 
@@ -11,7 +11,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(VARCHAR(250), nullable=False)
-    hashed_password = Column(VARCHAR(250), nullable=False)
-    session_id = Column(VARCHAR(250))
-    reset_token = Column(VARCHAR(250))
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
