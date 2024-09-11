42fb3a40803806a24baaed50e89b81c3b574ca09
dan <nyaxda@gmail.com>
2024-09-11 18:08:11 -0500

    chore: Add user registration endpoint to app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """app module"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +12,17 @@ app = Flask(__name__)
 def home():
     """basic Flask app"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "user already exists"}), 400
 
 
 if __name__ == "__main__":
