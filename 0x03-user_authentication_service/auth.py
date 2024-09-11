44ea31789f05760e92cf1d32b4d3b6113c7e7170
dan <nyaxda@gmail.com>
2024-09-11 17:41:49 -0500

    chore: Refactor Auth class to handle user registration and password hashing

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,32 @@
 """ Auth module
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        except InvalidRequestError:
+            pass
+        user = self._db.add_user(email,
+                                 _hash_password(password).decode('utf-8'))
+        return user
