5a11f7675da5bbb1b0a09e9bab11b731b2dbd0e7
dan <nyaxda@gmail.com>
2024-09-11 17:20:38 -0500

    feat: Add password hashing function using bcrypt

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
