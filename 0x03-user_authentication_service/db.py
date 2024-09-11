31d46713223bfb5c482db5cd5db4c8770312f376
Daniel Nyakundi <nyaxda@gmail.com>
2024-09-11 14:14:32 -0700

    Refactor query results to use .first() instead of .one()

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,9 +48,11 @@ class DB:
         if not kwargs:
             raise InvalidRequestError
         try:
-            user = self._session.query(User).filter_by(**kwargs).one()
-            return user
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
         except NoResultFound:
-            raise
-        except Exception:
+            raise NoResultFound
+        except Exception as e:
             raise InvalidRequestError
+        return user
