d6572b65a93b7ed6724d9439d58314756f3d8e76
dan <nyaxda@gmail.com>
2024-09-11 16:59:12 -0500

    Refactor find_user_by method in DB class to handle invalid requests and no results found

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,18 +44,13 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Finds a user based on a set of filters.
-        """
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = self._session.query(User).filter(
-            tuple_(*fields).in_([tuple(values)])
-        ).first()
-        if result is None:
-            raise NoResultFound()
-        return result
+    """Find a user by arbitrary keyword arguments."""
+    if not kwargs:
+        raise InvalidRequestError("No arguments provided")
+    try:
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+    except NoResultFound:
+        raise NoResultFound
