1ad9c25b331d209c8bc05d788745c90041c85ae1
dan <nyaxda@gmail.com>
2024-09-12 09:45:36 -0500

    docstrings

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,6 +66,7 @@ def profile():
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
+    """Get a reset password token"""
     email = request.form.get('email')
     try:
         reset_token = AUTH.get_reset_password_token('email')
