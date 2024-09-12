2b0b63c8281d200e68628c43d98b25e961fdcd86
dan <nyaxda@gmail.com>
2024-09-12 10:57:21 -0500

    codefix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,13 +84,18 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Updates Users password"""
+        """Updates a user's password given the user's reset token.
+        """
+        user = None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            new_password = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=new_password,
-                                 reset_token=None)
         except NoResultFound:
-            raise ValueError
-        except InvalidRequestError:
-            raise ValueError
+            user = None
+        if user is None:
+            raise ValueError()
+        new_password_hash = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=new_password_hash,
+            reset_token=None,
+        )
