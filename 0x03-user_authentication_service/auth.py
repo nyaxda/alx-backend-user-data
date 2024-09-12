887f2595dccb80877a68fc7dfc667ca49717659d
dan <nyaxda@gmail.com>
2024-09-12 10:37:14 -0500

    chore: Handle NoResultFound exception in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,5 +90,5 @@ class Auth:
             new_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=new_password,
                                  reset_token=None)
-        except Exception:
+        except NoResultFound:
             raise ValueError
