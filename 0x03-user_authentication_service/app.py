f358aa705dba623c35c284685960841d9de10072
dan <nyaxda@gmail.com>
2024-09-12 09:10:12 -0500

    chore: Refactor logout functionality in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,14 +42,14 @@ def login():
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
     """Logs out a user"""
-    cookies = request.cookies
-    session_id = cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        response = make_response(redirect(url_for('home')))
-        response.delete_cookie('session_id')
-        return response
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            response = make_response(redirect(url_for('home')))
+            response.delete_cookie('session_id')
+            return response
     return abort(403)
 
 
