7966a55e9cb17cd374cb4445179721b71985ee6c
dan <nyaxda@gmail.com>
2024-09-12 10:54:53 -0500

    chore: Update docstring for update_password method in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,7 +84,7 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Updates User's password"""
+        """Updates Users password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             new_password = _hash_password(password)
