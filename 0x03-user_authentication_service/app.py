f096f2fe51808529bf441e0f5cc3c5a1cdfa6b1f
dan <nyaxda@gmail.com>
2024-09-12 11:11:05 -0500

    chore: Update password update response status code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,7 +82,7 @@ def update_password():
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
 
