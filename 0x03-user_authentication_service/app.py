dca9fa5daf8047b637bdc24cf7ca9095fcc1058b
dan <nyaxda@gmail.com>
2024-09-12 09:05:42 -0500

    chore: Update logout functionality to delete session cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,7 +47,9 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect(url_for('home'))
+        response = make_response(redirect(url_for('home')))
+        response.delete_cookie('session_id')
+        return response
     return abort(403)
 
 
